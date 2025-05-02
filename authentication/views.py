9773ff14b588423d2ab52f725a4016a3c6d3ba79
pcthuoc <pcthuoch#gmail.com>
2025-05-02 23:55:05 +0700

    update neww

--- authentication/views.py
+++ authentication/views.py
@@ -27,7 +27,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("/customers/profile/")
+                return redirect("/")
             else:
                 msg = 'Invalid credentials'
         else:
